af6fc443a75f0f8c50eea1b0ab7038843f6e28e0
Matias <noemail@noemail.com>
2023-11-24 19:27:34 -0300

    handling form errors I

--- app/api/issues/schema.ts
+++ app/api/issues/schema.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 const schema = z.object({
-    title: z.string().min(1).max(255),
+    title: z.string().min(1, 'Title is not long enough').max(255),
     description: z.string().min(1),
 });
 export default schema;
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,18 +1,56 @@
 'use client';
 
-import { Button, TextField } from '@radix-ui/themes'
+import { Button, Callout, TextField } from '@radix-ui/themes'
+import { useForm, Controller } from 'react-hook-form';
+import { useRouter } from 'next/navigation';
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { useState } from 'react';
 
-const NewIssue = () => (
-    <div className='max-w-xl'>
-        <h1>New issue</h1>
-        <TextField.Root className='my-4'>
-            <TextField.Input placeholder="Title" />
-        </TextField.Root>
-        <SimpleMDE placeholder="Description" />
-        <Button>Submit</Button>
-    </div>
-)
+interface NewIssueForm {
+    title: string;
+    description: string;
+}
+
+const NewIssue = () => {
+    const { register, control, handleSubmit } = useForm<NewIssueForm>();
+    const router = useRouter();
+    const [error, setError] = useState('')
+
+    return (
+        <div className='max-w-xl'>
+            {
+                error && (
+                    <Callout.Root color="red" className='mb-2'>
+                        <Callout.Text>{error}</Callout.Text>
+                    </Callout.Root>)
+            }
+            <form onSubmit={handleSubmit(async (formData) => {
+                const options = {
+                    method: 'POST',
+                    body: JSON.stringify(formData),
+                };
+                const response = await fetch("/api/issues", options);
+                if (!response.ok)
+                    setError('An unexpected error occurred.');
+                else
+                    router.push('/issues');
+            }
+            )}>
+                <h1>New issue</h1>
+                <TextField.Root className='my-4'>
+                    <TextField.Input placeholder="Title" {...register('title')} />
+                </TextField.Root>
+                <Controller
+                    name="description"
+                    control={control}
+                    render={({ field }) => <SimpleMDE placeholder="Description" {...field} />}
+                />
+
+                <Button>Submit</Button>
+            </form>
+        </div>
+    )
+}
 
 export default NewIssue
