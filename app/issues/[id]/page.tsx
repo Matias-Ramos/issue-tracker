770d652fd90b31b43eb0e0e6d10f3fcb036337e6
MatiasRamos <mattsk97@gmail.com>
2023-12-04 16:54:12 -0300

    Delete btn added

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,8 +1,9 @@
 import prisma from "@/prisma/client";
-import { Box, Grid } from "@radix-ui/themes";
+import { Box, Flex, Grid } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import EditIssueBtn from "./EditIssueBtn";
 import IssueDetails from "./IssueDetails";
+import DeleteIssueBtn from "./DeleteIssueBtn";
 
 interface Props {
     params: { id: string };
@@ -23,12 +24,15 @@ const IssueDetailPage = async ({ params }: Props) => {
     /******************** */
     // Rendering
     return (
-        <Grid columns={{ initial: "1", md: "2" }} gap="5">
-            <Box>
+        <Grid columns={{ initial: "1", sm: "5" }} gap="5">
+            <Box className="md:col-span-5">
                 <IssueDetails issue={issue} />
             </Box>
             <Box>
-                <EditIssueBtn issueId={issue.id} />
+                <Flex direction="column" gap="4">
+                    <DeleteIssueBtn issueId={issue.id} />
+                    <EditIssueBtn issueId={issue.id} />
+                </Flex>
             </Box>
         </Grid>
     )
