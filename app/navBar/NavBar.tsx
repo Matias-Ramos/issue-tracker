bfe7948549fc782683bf188356e196f9537a508a
MatiasRamos <mattsk97@gmail.com>
2023-12-09 12:35:49 -0300

    login logout buttons implemented

--- app/navBar/NavBar.tsx
+++ app/navBar/NavBar.tsx
@@ -4,9 +4,12 @@ import Link from "next/link";
 import { FaBug } from "react-icons/fa";
 import { usePathname } from "next/navigation";
 import { pages, containerStyle, olStyle, getLinkClasses } from './utils';
+import { useSession } from "next-auth/react";
+import { Box } from "@radix-ui/themes";
 
 const NavBar = () => {
   const currentPath = usePathname();
+  const { status, data: session } = useSession();
 
   return (
     <div className={containerStyle}>
@@ -16,12 +19,20 @@ const NavBar = () => {
       <ol className={olStyle}>
         {pages.map((page, index) => (
           <li key={index}>
-            <Link href={page.link} className={getLinkClasses(page.link, currentPath)}>
+            <Link
+              href={page.link}
+              className={getLinkClasses(page.link, currentPath)}
+            >
               {page.name}
             </Link>
           </li>
         ))}
       </ol>
+      <Box>
+      { status === 'authenticated' && <Link href="/api/auth/signout"> Log Out</Link>}
+      { status === 'loading' && <span>Loading...</span>}
+      { status === 'unauthenticated' && <Link href='/api/auth/signin'> Login </Link>}
+      </Box>
     </div>
   );
 };
